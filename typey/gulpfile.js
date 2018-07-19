031bf8a9699e94a0a795f99e8be51ceec18525cd
danilosantoalvarado <danilosantosalvarado@gmail.com>
2018-07-19 17:21:09 -0500

    add npm typey

---
+++ typey/gulpfile.js
@@ -0,0 +1,64 @@
+var importOnce = require('node-sass-import-once'),path = require('path');  //importacion de paquetes node_modules 
+
+var options = {};
+var gulp      = require('gulp');
+var sass      = require('gulp-sass');
+
+// #############################
+// Edit these paths and options.
+// #############################
+
+// The root paths are used to construct all the other paths in this
+// configuration. The "project" root path is where this gulpfile.js is located.
+// While Zen distributes this in the theme root folder, you can also put this
+// (and the package.json) in your project's root folder and edit the paths
+// accordingly.
+options.rootPath = {
+  project     : __dirname + '/',
+  styleGuide  : __dirname + '/typey/',
+  theme       : __dirname + '/'
+};
+
+options.theme = {
+  root  : options.rootPath.theme,
+  css   : options.rootPath.theme + 'css/',
+  sass  : options.rootPath.theme + 'sass/',
+  js    : options.rootPath.theme + 'js/'
+};
+
+// Define the node-sass configuration. The includePaths is critical!
+options.sass = {
+  importer: importOnce,
+  includePaths: [
+    options.theme.sass,
+    options.rootPath.project + 'node_modules/typey/stylesheets',
+  ],
+  outputStyle: 'expanded'
+};
+
+// ##########
+// Build CSS.
+// ##########
+var sassFiles = [
+  options.theme.sass + '**/*.scss',
+  // Do not open Sass partials as they will be included as needed.
+  '!' + options.theme.sass + '**/_*.scss'
+];
+
+gulp.task('styles', function() {
+  return gulp.src(sassFiles)
+    .pipe(sass(options.sass).on('error', sass.logError))
+    .pipe(gulp.dest(options.theme.css))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles']);
+
+
+// Resources used to create this gulpfile.js:
+// - https://github.com/google/web-starter-kit/blob/master/gulpfile.babel.js
+// - https://github.com/dlmanning/gulp-sass/blob/master/README.md
+// - http://www.browsersync.io/docs/gulp/
